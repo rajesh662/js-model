6168563abd576ddedb7f64602ca4f87c924edd46
rajesh662 <rajkuttyrajesh56@gmail.com>
2023-03-08 14:39:06 +0530

    upload

---
+++ main.js
@@ -0,0 +1,22 @@
+// let section = document.querySelector(".section")
+
+// let div1 = document.createElement("div");
+// section.appendChild(div1);
+// div1.classList.add("container");
+
+// let div2 = document.createElement("div");
+// div1.appendChild(div2);
+// div2.classList.add("toggle-cont");
+
+// let p = document.createElement("p");
+// div2.appendChild(p);
+
+// console.log(section);
+let add = document.querySelector(".container");
+function on1(){
+    add.style.display="block";
+}
+
+function off(){
+    add.style.display="none";
+}
